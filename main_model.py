201f024fc307b0c4b37cf6e9313e38d108186d7e
Shivamshaiv <patelshivam99@gmail.com>
2019-07-10 21:44:54 -0400

    Addded the basic before init version.

---
+++ main_model.py
@@ -0,0 +1,43 @@
+import numpy as np
+import math
+
+class DICE_model:
+    def __init__(self,social_time_prefrence_rate_rho=0.015,elasticity_marginal_utility_alpha=1.5,
+    coef_on_damage_exponent_pi2 = 0.0028,damage_exponent_epsilon = 2,exponent_emission_reduction_theta2 = 2.8,production_gamma=0.300):
+        self.time = 1
+        self.L = [6411]                                                                                   # World Population in millions
+        self.c = [self.C[0]/self.L[0]]                                                                        # per capita consumption, thosands of 2005 US dollars
+        self.C = [self.Q[0] - self.I[0]]                                                                      # Total consumption, trillions of 2005 US dollars
+        self.R = [1/(1+social_time_prefrence_rate)]                                                     # Social Time Discount factor
+        self.U = [(np.pow(self.c[0],1-elasticity_marginal_utility))/(1-elasticity_marginal_utility) + 1]
+
+
+        self.A = [0.0303220]
+        self.K = [137]                      #  2005 capital value
+        self.sigma_g = [0.158]              #  Initial rate of decline of carbon intensity per period
+        self.sigma = [0.14452]              #  2005 Effective Carbon Intensity
+        self.T_at = 0.83                                                                                # Temperature change from 1900 until 2000
+        self.omega = [1 - (1/(1+(coef_on_damage_exponent_pi2*pow(self.T_at,damage_exponent_epsilon))))]
+        self.BC = [1.26]                    # Cost of backstop technology
+        self.sigma =
+        self.mu = [0]
+        self.Eind = [(self.sigma[0])*(1-self.mu[0])*productivity(self.A[0],self.K[0],self.L[0],production_gamma)]
+        self.lmbda = [(np.pow(abatement_phi(self.time),1-exponent_emission_reduction_theta2)*self.BC[0]*
+        np.pow(self.mu[0],exponent_emission_reduction_theta2)*self.sigma[0])/exponent_emission_reduction_theta2]
+        self.Y =
+        #self.Q = [((1 - self.omega)*(1 - self.lmbda))*self.Y]
+        self.Q = [55.34]
+
+
+    def abatement_phi(time,phi0=1,phi5=1,phi10=1,phi15=1,phimax=1):
+        if time < 5:
+            return phi5 + (phi0-phi5)*np.exp(-0.25*time)
+        elif time >= 5 and time < 10 :
+            return phi10 + (phi5-phi10)*np.exp(-0.25*time)
+        elif time >= 10 and time < 15 :
+            return phi15 + (phi10-phi15)*np.exp(-0.25*time)
+        else:
+            return phi15 + (phimax-phi15)*np.exp(-0.25*time)
+
+    def productivity(A,K,L,gamma):
+        return A*np.pow(K,gamma)*np.pow(L,1-gamma)
