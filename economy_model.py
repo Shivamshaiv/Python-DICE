e9d2bb3d7fe42f71114f8bb7c074ac22d6103d61
Shivamshaiv <patelshivam99@gmail.com>
2019-07-16 13:38:39 -0400

    Completed the business as usual version of DICE for 10 year time steps.

--- economy_model.py
+++ economy_model.py
@@ -21,10 +21,10 @@ class Economy:
         self.K = [137]                      #  2005 capital value
         self.sigma_g = [0.158]              #  Initial rate of decline of carbon intensity per period
         self.sigma = [0.14452]              #  2005 Effective Carbon Intensity
-        self.T_at = [0.83]                    #  Temperature change from 1900 until 2000                                                           # Temperature change from 1900 until 2000
+        self.T_at = [0.83]                    #  Temperature change from 1900 until 2000
         self.omega = [1 - (1/(1+(coef_on_damage_exponent_pi2*np.power(self.T_at[0],damage_exponent_epsilon))))]
         self.BC = [1.26]                    # Cost of backstop technology
-        self.mu = [0]
+        self.mu = [1]
         self.Eind = [(self.sigma[0])*(1-self.mu[0])*productivity(self.A[0],self.K[0],self.L[0],production_gamma)]
         self.lmbda = [(np.power(abatement_phi(self.time),1-exponent_emission_reduction_theta2)*self.BC[0]*
         np.power(self.mu[0],exponent_emission_reduction_theta2)*self.sigma[0])/exponent_emission_reduction_theta2]
@@ -35,8 +35,10 @@ class Economy:
         self.C = [self.Q[0] - self.I[0]]                                                # Total consumption, trillions of 2005 US dollars
         self.c = [self.C[0]/self.L[0]]                                                              # per capita consumption, thosands of 2005 US dollars
         self.U = [(np.power(self.c[0],1-elasticity_marginal_utility_alpha))/(1-elasticity_marginal_utility_alpha) + 1]
+        self.test = []
 
-        self.E_ind = [self.sigma[0]*(1-self.mu[0])*self.Y[0]]
+        #self.E_ind = [self.sigma[0]*(1-self.mu[0])*self.Y[0]]
+        self.E_ind = [84.1910-1.1]
         self.E_land = [1.1]                                                          # Carbon emissions from land use (ie, deforestation), GtC per period
         self.E = [84.1910]
         self.Tax_tau = self.BC[0]*pow(self.mu[0],exponent_emission_reduction_theta2-1)  # Carbon Tax
@@ -49,6 +51,8 @@ class Economy:
         self.F_ex = [0.83]
         self.F_ex0 = self.F_ex[0]                                                         #  Non-CO2 forcings in 2005
         self.F_ex10 = 0.30                                                        #  Estimate of non-CO2 forcings in 2100
+
+        self.W = [self.L[-1]*self.U[-1]*self.R[-1]]
 
         ##### Connstants ############
         self.social_time_prefrence_rate_rho = 0.015
@@ -78,48 +82,11 @@ class Economy:
         self.F = [self.F2CO2*(np.log2(self.M_at[0]/self.preindustrail_carbon_Mpi)) + self.F_ex0]       # Forcing due to CO2
 
 
-    def abatement_phi(time,phi0=1,phi5=1,phi10=1,phi15=1,phimax=1):
-        if time < 5:
-            return phi5 + (phi0-phi5)*np.exp(-0.25*time)
-        elif time >= 5 and time < 10 :
-            return phi10 + (phi5-phi10)*np.exp(-0.25*time)
-        elif time >= 10 and time < 15 :
-            return phi15 + (phi10-phi15)*np.exp(-0.25*time)
-        else:
-            return phi15 + (phimax-phi15)*np.exp(-0.25*time)
-
-    def productivity(A,K,L,gamma):
-        return A*np.power(K,gamma)*np.power(L,1-gamma)
-
-    def compute_external_forcing(self,time):
-        if time > 10:
-            return self.F_ex10
-        else:
-            return self.F_ex0 + 0.1*(self.F_ex0-self.F_ex10)*time
-
-    def update_carbon_masses(self):
-        _b11, _b12, _b13 = 0.810712, 0.189288, 0
-        _b21, _b22, _b23 = 0.097213, 0.852787, .05
-        _b31, _b32, _b33 = 0, 0.003119, 0.996881
-        self.carbon_matrix = np.array([
-            _b11, _b12, _b13,
-            _b21, _b22, _b23,
-            _b31, _b32, _b33,
-        ]).reshape(3, 3)
-
-        self.mass_matrix = np.array([[self.M_at[-1]],[self.M_up[-1]],[self.M_lo[-1]]])
-        self.emmision_matrix = np.array([[self.E[-1]],[0],[0]])
-
-        new_masses = np.matmul(self.carbon_matrix,self.mass_matrix) + self.emmision_matrix
-
-        self.M_at.append(new_masses[0][0])
-        self.M_up.append(new_masses[1][0])
-        self.M_lo.append(new_masses[2][0])
-
     def loop(self,t=1):
         for time in range(t):
             self.time = time
             self.L.append(np.sqrt(self.L[-1]*self.L_Tmax))
+            self.R.append(1/np.power(1+self.social_time_prefrence_rate_rho,self.time))
             self.T_lo.append(self.T_lo[-1]+(self.ξ3*(self.T_at[-1]-self.T_lo[-1])))
             const_lamb = self.F2CO2/self.temp_increase_doubling_co2
             self.F_ex.append(compute_external_forcing(self,time))
@@ -134,27 +101,46 @@ class Economy:
             #Update productivity
             self.Y.append(productivity(self.A[-1],self.K[-1],self.L[-1],self.production_gamma))
 
+            self.sigma_g.append(self.sigma_g[-1]*(1-self.decline_rate_decarbonisation_σd1))
+            self.sigma.append(self.sigma[-1]*(1-self.sigma_g[-2]))
 
             # Emisssions
             self.E_land.append(self.E_land[0]*np.power(0.8,time))
+            self.E_ind.append(self.sigma[-1]*1*self.Y[-1])
+            self.E.append(self.E_land[-1]+self.E_ind[-1])
 
             update_carbon_masses(self)
-            F_eta = 3.8    #Forced CO2 doubling.
+            F_eta = 1.5    #Elasticity Demand
             self.F.append(F_eta*(np.log2(self.M_at[-1]/self.preindustrail_carbon_Mpi)) + self.F_ex[-1])
 
             self.T_at.append(self.T_at[-1]+self.ξ1*(self.F[-1]-const_lamb*self.T_at[-1]-self.ξ2*(self.T_at[-1]-self.T_lo[-2])))
             self.omega.append(1 - (1/(1+(self.coef_on_damage_exponent_pi2*np.power(self.T_at[-1],self.damage_exponent_epsilon)))))
 
             ### Abetement Function #To do
-            self.BC.append(self.BC[0]*np.power(1-self.costdecinline_backstop_tech_percent_BCg))
+            self.BC.append(self.BC[0]*np.power(1-self.costdecinline_backstop_tech_percent_BCg,self.time))
 
-            self.lmbda.append[(np.power(abatement_phi(self.time),1-exponent_emission_reduction_theta2)*self.BC[-1]*
-            np.power(self.mu[-1],exponent_emission_reduction_theta2)*self.sigma[-1])/exponent_emission_reduction_theta2]
+            self.lmbda.append((np.power(abatement_phi(self.time),1-self.exponent_emission_reduction_theta2)*self.BC[-1]*np.power(self.mu[-1],self.exponent_emission_reduction_theta2)*self.sigma[-1])/self.exponent_emission_reduction_theta2)
+
+            # Update Q production Function
+            self.Q.append((1-self.omega[-1])*(1-self.lmbda[-1])*self.Y[-1])
+
+            # Update the Investment.
+            self.I.append(self.saving_rate_s*self.Q[-1])
+
+            #Update the capital
+            self.C.append(self.Q[-1]-self.I[-1])
+            self.c.append(self.C[-1]/self.L[-1])
 
 
+            self.test.append((1-self.omega[-1])*(1-self.lmbda[-1]))
+
+'''
 model = Economy()
+print(model.K[-1])
 #print(model.__dict__)
 model.loop(100)
 #print(model.__dict__)
-plt.plot(model.K)
+plt.plot(model.A_g)
+print(model.Q)
 plt.show()
+'''
